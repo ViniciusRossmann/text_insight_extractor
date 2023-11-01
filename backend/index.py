e810aef334fda46ff2786b00f32850378d3631e1
Vinicius Nunes <viniciusrossmann@gmail.com>
2023-11-01 20:49:48 -0300

    ajusta consumo do modelo

--- backend/index.py
+++ backend/index.py
@@ -1,12 +1,23 @@
 import pickle
+import re
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 
 model = pickle.load(open("./models/LogisticRegressionTextClassifier", 'rb'))
 vectorizer = pickle.load(open("./models/vectorizer", 'rb'))
 
 app = Flask(__name__)
 CORS(app)
+
+def preprocess_text(text):
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    words = text.lower().split()
+    words = [word for word in words if word not in stopwords.words('portuguese')]
+    stemmer = SnowballStemmer('portuguese')
+    words = [stemmer.stem(word) for word in words]
+    return ' '.join(words)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,21 +26,18 @@ def home():
 @app.route('/classifications', methods=['POST'])
 def classify():
     data = request.get_json()
-
-    print(data)
     
     if 'text' not in data:
-        return jsonify({'erro': 'O campo "texto" é obrigatório'}), 400
+        return jsonify({'error': 'O campo "texto" é obrigatório'}), 400
 
     texto = data['text']
     linhas = texto.split('\n')
+    linhas_processadas = [preprocess_text(item) for item in linhas]
     
-    text_vectorizer = vectorizer.transform(linhas)
+    text_vectorizer = vectorizer.transform(linhas_processadas)
     predictions = model.predict(text_vectorizer)
     
-    resultado = {}
-    for i, linha in enumerate(linhas, start=0):
-        resultado[linha] = predictions[i]
+    resultado = [{"text": linha, "classification": predictions[i]} for i, linha in enumerate(linhas) if linha]
     return jsonify(resultado)
 
 if __name__ == "__main__":
